d96396259420a25dab5af7ba97233442be0ece49
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-05-18 10:58:58 -0300

    adicionando código base dos teste

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+jest.mock("react-redux", () => ({
+ 
+}));
+
+test("should exist in the document", () => {
+  
+});
+
+test("should click in button", () => {
+ 
+});
+
+test("should click in button and trigger event", async () => {
+ 
+});
